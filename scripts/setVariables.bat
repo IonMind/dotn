7265890205fad86b48a5aeea97ec77613962e095
abhishek_chaudhary <abhishek.chaudhary@highlands.in>
2021-03-09 15:31:24 +0530

    z

--- scripts/setVariables.bat
+++ scripts/setVariables.bat
@@ -1,12 +1,14 @@
-set PULL_PLAN_PATH_WIN=%Build.SourcesDirectory%
+@echo off
+echo Hello World from %AGENT_NAME%
+set PULL_PLAN_PATH_WIN=$(Build.SourcesDirectory)
 set PULL_PLAN_PATH_WIN=%PULL_PLAN_PATH_WIN:\=\\%
-set PRODUCTVERSION=%MajorVersion%.%MinorVersion%.0.%Build.BuildNumber%
+set PRODUCTVERSION=$(MajorVersion).$(MinorVersion).0.$(Build.BuildNumber)
 
 
-for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`% do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set TodayDate=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%
 echo %TodayDate%
 set DateTime=%ldt:~4,2%/%ldt:~6,2%/%ldt:~0,4% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,2%
 echo %DateTime%
-set ExeName=%ExeNamePrefix%SalesCloser360 %TodayDate%.exe
+set ExeName=$(ExeNamePrefix)SalesCloser360 %TodayDate%.exe
 
