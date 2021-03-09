dff2e80d30831239dacc73774cf491cc9b88cd68
abhishek_chaudhary <abhishek.chaudhary@highlands.in>
2021-03-09 15:32:46 +0530

    asf

--- scripts/setVariables.bat
+++ scripts/setVariables.bat
@@ -3,7 +3,7 @@ echo Hello World from %AGENT_NAME%
 set PULL_PLAN_PATH_WIN=$(Build.SourcesDirectory)
 set PULL_PLAN_PATH_WIN=%PULL_PLAN_PATH_WIN:\=\\%
 set PRODUCTVERSION=$(MajorVersion).$(MinorVersion).0.$(Build.BuildNumber)
-
+echo %PULL_PLAN_PATH_WIN%
 
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set TodayDate=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%
