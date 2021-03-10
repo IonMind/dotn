b2f1fd3145200c2b25733f4eedbcfcc32ae99476
abhishek_chaudhary <abhishek.chaudhary@highlands.in>
2021-03-10 14:07:40 +0530

    check

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -1,2 +1,4 @@
 rm -rf Source;
-git clone "/mnt/c/Users/abhishek.chaudhary/Documents/All Repos/salescloser360" "./Source/Sales_Closer_360";
+echo $EXENAMEPREFIX
+echo $SMART_ASSEMBLY_PATH_DIR
+#git clone "/mnt/c/Users/abhishek.chaudhary/Documents/All Repos/salescloser360" "./Source/Sales_Closer_360";
