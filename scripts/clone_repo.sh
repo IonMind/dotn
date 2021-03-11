0ca032fe11d6b51744b75525607cc6e2560fb53d
abhishek_chaudhary <abhishek.chaudhary@highlands.in>
2021-03-11 18:18:17 +0530

    m2

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -2,6 +2,9 @@ if [ "$BUILD.SOURCEBRANCH"  == "refs/heads/master" ]
 then
     echo "master branch"
 fi
-echo $BUILD.SOURCEBRANCH
+echo $BUILD_SOURCEBRANCH
+echo $Build_SourceBranchName
+echo $Agent_WorkFolder
+mkdir 'Agent_WorkFolder/$Build_SourceBranchName
 rm -rf Source;
-git clone "C:\Users\abhishek.chaudhary\Documents\All Repos\salescloser360" "./Source/Sales_Closer_360";
+git clone "C:\Users\abhishek.chaudhary\Documents\All Repos\salescloser360" "../Source/Sales_Closer_360";
