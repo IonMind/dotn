d29051a6ea4466ba9a1160d15fdbad51839e71e3
abhishek_chaudhary <abhishek.chaudhary@highlands.in>
2021-03-12 17:13:40 +0530

    id

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -6,5 +6,6 @@ echo $BUILD_SOURCEBRANCH
 echo $BUILD_SOURCEBRANCHNAME
 echo $AGENT_WORKFOLDER
 echo $BUILD_REPOSITORY_LOCALPATH
+echo $BUILD_BUILDID
 rm -rf Source
 git clone "C:\Users\abhishek.chaudhary\Documents\All Repos\salescloser360" "./Source/Sales_Closer_360";
